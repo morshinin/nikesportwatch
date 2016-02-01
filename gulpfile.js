f0d6e301a201270c05120b65cdd3f21e61d1f8ad
Mikhail Morshinin <314159265absurdus@gmail.com>
2016-02-01 20:35:08 +0300

    Saved some images from psd

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
+// #VARS
+
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var sass = require('gulp-sass');
 var csslint = require('gulp-csslint');
 
+var magician = require('postcss-font-magician');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
 
 var jade = require('gulp-jade');
 
+// #CSS
+
 gulp.task('css', function() {
   var processors = [
+    magician,
     autoprefixer,
     cssnano
   ];
@@ -19,14 +25,25 @@ gulp.task('css', function() {
     .pipe(gulp.dest('./build'));
 });
 
+// # LINT
+
 gulp.task('lint', function() {
   gulp.src('./build/*.css')
     .pipe(csslint())
     .pipe(csslint.reporter());
 });
 
+// #HTML
+
 gulp.task('html', function() {
   gulp.src('./source/jade/**/*.jade')
     .pipe(jade({pretty:true}))
     .pipe(gulp.dest('./build'));
+});
+
+// #WATCH
+
+gulp.task('watch', function() {
+  gulp.watch('./source/sass/**/*.scss', ['css']);
+  gulp.watch('./source/jade/**/*.jade', ['html']);
 });
